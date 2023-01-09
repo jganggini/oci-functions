60380385138703fa91bbdc3c2deb8a7d2bef9b05
☁️ Cloud Digital Architecture <je.gg.garcia24@outlook.com>
2023-01-08 21:42:25 -0500

    update

--- oci-serviceconnector-streaming-dml-adb-in-python/_utility/utl_base64_encode.py
+++ oci-serviceconnector-streaming-dml-adb-in-python/_utility/utl_base64_encode.py
@@ -4,18 +4,19 @@ import base64
 #           Parameter          #
 ################################
 # [Parameter:utl] Message
-par_message_key_01     = '16102234_2016-06-09 00:00:00_9977370'
-par_message_value_01   = '{"table": "LICMEDRAW.SIL_DEPOSITO", "op_type": "I", "op_ts": "2022-11-15 08:50:10.000000", "current_ts": "2022-11-15 08:50:10.100000", "pos": "00000000050010734001", "after": {"PER_RUT": 25102234, "DEP_FECINI": "2016-06-09 00:00:00", "FML_FORMUL": "3", "LIC_NUMLIC": 1077370, "DEP_DIALIC": "null", "TCT_TIPCTA": "V", "BAN_CODBAN": 12, "DEP_CTABAN": "123456789", "DEP_CORREOE": "correo@dominio.com", "DEP_AREAFONO": "32", "DEP_FONO": 1422300, "DEP_AREAFONO_COM": "null", "DEP_FONO_COM": "null", "DEP_CELULAR": 10788810, "DEP_FECTERVIGEN": "null", "DEP_LUGAR": "null", "DEP_ESTADO": "GRA", "DEP_FECGRA": "2016-07-05 21:17:33", "DEP_USUGRA": "ATRXLM", "DEP_FECACT": "2016-07-05 21:17:33", "DEP_USUACT": "ATRXLM", "DEP_CANAL": "null"}}'
+par_message_key_01   = '16102234_2016-06-09 00:00:00_9977370'
+par_message_value_01 = '{"table": "LICMEDRAW.SIL_DEPOSITO", "op_type": "I", "op_ts": "2022-11-15 08:50:10.000000", "current_ts": "2022-11-15 08:50:10.100000", "pos": "00000000050010734001", "after": {"PER_RUT": 25102234, "DEP_FECINI": "2016-06-09 00:00:00", "FML_FORMUL": "3", "LIC_NUMLIC": 1077370, "DEP_DIALIC": "null", "TCT_TIPCTA": "V", "BAN_CODBAN": 12, "DEP_CTABAN": "123456789", "DEP_CORREOE": "correo@dominio.com", "DEP_AREAFONO": "32", "DEP_FONO": 1422300, "DEP_AREAFONO_COM": "null", "DEP_FONO_COM": "null", "DEP_CELULAR": 10788810, "DEP_FECTERVIGEN": "null", "DEP_LUGAR": "null", "DEP_ESTADO": "GRA", "DEP_FECGRA": "2016-07-05 21:17:33", "DEP_USUGRA": "ATRXLM", "DEP_FECACT": "2016-07-05 21:17:33", "DEP_USUACT": "ATRXLM", "DEP_CANAL": "null"}}'
 
-par_message_key_02     = '16102234_2016-06-09 00:00:00_9977370'
-par_message_value_02   = '{"table": "LICMEDRAW.SIL_DEPOSITO", "op_type": "U", "op_ts": "2022-11-15 08:50:20.000000", "current_ts": "2022-11-15 08:50:20.100000", "pos": "00000000050010734002", "before": {"PER_RUT": 25102234, "DEP_FECINI": "2016-06-09 00:00:00", "FML_FORMUL": "3", "LIC_NUMLIC": 1077370, "DEP_DIALIC": "null", "TCT_TIPCTA": "V", "BAN_CODBAN": 12, "DEP_CTABAN": "123456789", "DEP_CORREOE": "correo@dominio.com", "DEP_AREAFONO": "32", "DEP_FONO": 1422300, "DEP_AREAFONO_COM": "null", "DEP_FONO_COM": "null", "DEP_CELULAR": 10788810, "DEP_FECTERVIGEN": "null", "DEP_LUGAR": "null", "DEP_ESTADO": "GRA", "DEP_FECGRA": "2016-07-05 21:17:33", "DEP_USUGRA": "ATRXLM", "DEP_FECACT": "2016-07-05 21:17:33", "DEP_USUACT": "ATRXLM", "DEP_CANAL": "null"}, "after": {"PER_RUT": 25102234, "DEP_FECINI": "2016-06-09 00:00:00", "LIC_NUMLIC": 1077370, "DEP_CELULAR": 20788820, "DEP_ESTADO": "N"}}'
+par_message_key_02   = '16102234_2016-06-09 00:00:00_9977370'
+par_message_value_02 = '{"table": "LICMEDRAW.SIL_DEPOSITO", "op_type": "U", "op_ts": "2022-11-15 08:50:20.000000", "current_ts": "2022-11-15 08:50:20.100000", "pos": "00000000050010734002", "before": {"PER_RUT": 25102234, "DEP_FECINI": "2016-06-09 00:00:00", "FML_FORMUL": "3", "LIC_NUMLIC": 1077370, "DEP_DIALIC": "null", "TCT_TIPCTA": "V", "BAN_CODBAN": 12, "DEP_CTABAN": "123456789", "DEP_CORREOE": "correo@dominio.com", "DEP_AREAFONO": "32", "DEP_FONO": 1422300, "DEP_AREAFONO_COM": "null", "DEP_FONO_COM": "null", "DEP_CELULAR": 10788810, "DEP_FECTERVIGEN": "null", "DEP_LUGAR": "null", "DEP_ESTADO": "GRA", "DEP_FECGRA": "2016-07-05 21:17:33", "DEP_USUGRA": "ATRXLM", "DEP_FECACT": "2016-07-05 21:17:33", "DEP_USUACT": "ATRXLM", "DEP_CANAL": "null"}, "after": {"PER_RUT": 25102234, "DEP_FECINI": "2016-06-09 00:00:00", "LIC_NUMLIC": 1077370, "DEP_CELULAR": 20788820, "DEP_ESTADO": "N"}}'
 
-par_message_key_03     = '16102234_2016-06-09 00:00:00_9977370'
-par_message_value_03   = '{"table": "LICMEDRAW.SIL_DEPOSITO", "op_type": "D", "op_ts": "2022-11-15 08:50:30.000000", "current_ts": "2022-11-15 08:50:30.100000", "pos": "00000000050010734003", "before": {"PER_RUT": 25102234, "DEP_FECINI": "2016-06-09 00:00:00", "FML_FORMUL": "3", "LIC_NUMLIC": 1077370, "DEP_DIALIC": "null", "TCT_TIPCTA": "V", "BAN_CODBAN": 12, "DEP_CTABAN": "123456789", "DEP_CORREOE": "correo@dominio.com", "DEP_AREAFONO": "32", "DEP_FONO": 1422300, "DEP_AREAFONO_COM": "null", "DEP_FONO_COM": "null", "DEP_CELULAR": 20788820, "DEP_FECTERVIGEN": "null", "DEP_LUGAR": "null", "DEP_ESTADO": "N", "DEP_FECGRA": "2016-07-05 21:17:33", "DEP_USUGRA": "ATRXLM", "DEP_FECACT": "2016-07-05 21:17:33", "DEP_USUACT": "ATRXLM", "DEP_CANAL": "null"}}'
+par_message_key_03   = '16102234_2016-06-09 00:00:00_9977370'
+par_message_value_03 = '{"table": "LICMEDRAW.SIL_DEPOSITO", "op_type": "D", "op_ts": "2022-11-15 08:50:30.000000", "current_ts": "2022-11-15 08:50:30.100000", "pos": "00000000050010734003", "before": {"PER_RUT": 25102234, "DEP_FECINI": "2016-06-09 00:00:00", "FML_FORMUL": "3", "LIC_NUMLIC": 1077370, "DEP_DIALIC": "null", "TCT_TIPCTA": "V", "BAN_CODBAN": 12, "DEP_CTABAN": "123456789", "DEP_CORREOE": "correo@dominio.com", "DEP_AREAFONO": "32", "DEP_FONO": 1422300, "DEP_AREAFONO_COM": "null", "DEP_FONO_COM": "null", "DEP_CELULAR": 20788820, "DEP_FECTERVIGEN": "null", "DEP_LUGAR": "null", "DEP_ESTADO": "N", "DEP_FECGRA": "2016-07-05 21:17:33", "DEP_USUGRA": "ATRXLM", "DEP_FECACT": "2016-07-05 21:17:33", "DEP_USUACT": "ATRXLM", "DEP_CANAL": "null"}}'
 
 # [Parameter:adb] Autonomous Database
-par_oci_adb_user_name   = 'ADMIN'
-par_oci_adb_password    = '*********'
+par_oci_adb_user_name       = 'ADMIN'
+par_oci_adb_password        = '*****'
+par_oci_adb_wallet_password = '*****'
 
 def base64_encode(encoded):
     message_bytes = encoded.encode('utf-8')
@@ -38,5 +39,6 @@ print('[OCI Vault]')
 print('================================================================')
 print('par_secret_user==>\n'+base64_encode(par_oci_adb_user_name))
 print('par_secret_password==>\n'+base64_encode(par_oci_adb_password))
+print('par_oci_adb_wallet_password==>\n'+base64_encode(par_oci_adb_wallet_password))
 
 #python utl_base64_encode.py
--- oci-serviceconnector-streaming-dml-adb-in-python/func.py
+++ oci-serviceconnector-streaming-dml-adb-in-python/func.py
@@ -1,18 +1,17 @@
-import os
-import io
-import oci
-import json
 import base64
-import zipfile
-import tempfile
+import io
+import json
 import logging
-import pandas as pd
-
-import cx_Oracle
 import pathlib
+import pandas as pd
 import pytest
-from fdk import response
+import os
+import oci
+import oracledb
+import zipfile
+
 from fdk import fixtures
+from fdk import response
 from time import process_time
 from pandas import json_normalize
 
@@ -23,61 +22,53 @@ class Utility():
         message_bytes = base64.b64decode(base64_bytes)
         return message_bytes.decode('utf-8')
 
-    # Retrieve secret
-    def read_secret_value(secret_id):
+    # Retrieve Secret
+    def read_secret_value(secret_ocid):
         logger = logging.getLogger()        
         try:
             # Get instance principal context
             if (oci.config.validate_config(par_oci_config) is None):
                 secret_client = oci.secrets.SecretsClient(par_oci_config)                
-                response = secret_client.get_secret_bundle(secret_id)
+                response = secret_client.get_secret_bundle(secret_ocid)
                 base64_Secret_content = response.data.secret_bundle_content.content
 
                 return Utility.base64_decode(base64_Secret_content)
         except Exception as e:
             logger.error(str(e))
             raise
-
-    def get_select_before(input_df):
-        before_list = []
-        for col in input_df.columns:
-            if col[0:7] == "before.":
-                before_list.append(col)
-        return input_df[before_list].rename(lambda x: x.replace("before.", ""), axis="columns")
     
-    def get_select_after(input_df):
-        after_list = []
-        for col in input_df.columns:
-            if col[0:6] == "after.":
-                after_list.append(col)
-        return input_df[after_list].rename(lambda x: x.replace("after.", ""), axis="columns")
+    # Extract files in wallet zip to a directory
+    def zipfile(file_zip_dir, temp_dir):
+       zipfile.ZipFile(file_zip_dir).extractall(temp_dir)
 
-    def set_update_before(table_unique_key,df_before, df_after):
-        df_before.set_index(table_unique_key, inplace=True)
-        df_before.update(df_after.set_index(table_unique_key))
-        return df_before.reset_index()
+    # Remove files from directory
+    def remove_files_from_directory(temp_dir):
+        for f in os.listdir(temp_dir):
+            os.remove(os.path.join(temp_dir, f))
 
 ################################
 #           Parameter          #
 ################################
 # [Parameter:utl] Utility
-utl_path                          = str(pathlib.Path(__file__).parent.absolute())
+utl_path                                = str(pathlib.Path(__file__).parent.absolute())
 # [Parameter:oci]
-par_oci_config                    = oci.config.from_file(utl_path + '/_oci/config')
+par_oci_config                          = oci.config.from_file(utl_path + '/_oci/config')
 # [Parameter:oci_adb] Autonomous Database
-par_oci_adb_user_name_secret_ocid = Utility.read_secret_value('ocid1.vaultsecret.oc1.iad.amaaaaaavoaa5ziaifr*****************************************')
-par_oci_adb_password_secret_ocid  = Utility.read_secret_value('ocid1.vaultsecret.oc1.iad.amaaaaaavoaa5zian2s*****************************************')
-par_oci_adb_service_name          = "atpemo_high"
-par_oci_adb_wallet_location       = utl_path + '/wallet/Wallet_atpdemo.zip'
-# [Parameter:tbl] Table
-par_oci_tbl_unique_key            = ["PER_RUT", "DEP_FECINI", "LIC_NUMLIC"]
+par_oci_adb_user_name_secret_ocid       = Utility.read_secret_value('ocid1.vaultsecret.oc1.iad.amaaaaaavoaa5zia5h2xoqogjl7eacvubji7eyax3orafftra6eidfhur2ma')
+par_oci_adb_password_secret_ocid        = Utility.read_secret_value('ocid1.vaultsecret.oc1.iad.amaaaaaavoaa5zian2sgagsiqm5wdsyahbfn7ktylihyapk5v4zhvtaniema')
+par_oci_adb_wallet_password_secret_ocid = Utility.read_secret_value('ocid1.vaultsecret.oc1.iad.amaaaaaavoaa5ziaddhnxt56tjso6souyodoj2erffa2h3vmswhpzidd6f7q')
+par_oci_adb_service_name                = "adwdemo_high"
+par_oci_adb_wallet_location             = utl_path + '/_oci/adb_wallet/Wallet_adwdemo.zip'
+par_oci_adb_wallet_temp_dir             = utl_path + '/_oci/adb_wallet/zipfile_temp'
+# [Parameter:oci_str] OCI Streaming
+par_oci_str_tbl_unique_key              = ["PER_RUT", "DEP_FECINI", "LIC_NUMLIC"]
 
 ################################
 #    Controlador de Eventos    #
 ################################
 def handler(ctx, data: io.BytesIO=None):
     logger = logging.getLogger()
-
+    
     try:
         logs = json.loads(data.getvalue())
         logger.info('Received {} entries.'.format(len(logs)))
@@ -97,13 +88,13 @@ def handler(ctx, data: io.BytesIO=None):
             var_dml = str(df_value['op_type'][0])
             
             if(var_dml=='I'):
-                var_df = Utility.get_select_after(df_value)
+                var_df = oci_str.get_select_after(df_value)
             elif (var_dml=='U'):
-                df_after = Utility.get_select_after(df_value)
-                df_before = Utility.get_select_before(df_value)
-                var_df = Utility.set_update_before(par_oci_tbl_unique_key, df_before, df_after)
+                df_after = oci_str.get_select_after(df_value)
+                df_before = oci_str.get_select_before(df_value)
+                var_df = oci_str.set_update_before(par_oci_str_tbl_unique_key, df_before, df_after)
             elif (var_dml=='D'):
-                var_df = Utility.get_select_before(df_value)
+                var_df = oci_str.get_select_before(df_value)
 
             var_value = var_df.to_json(orient='records')[1:-1].replace('},{', '} {')
 
@@ -112,7 +103,9 @@ def handler(ctx, data: io.BytesIO=None):
 
             # Execute procedure from Autnomous Database
             logger.info('[INI]['+ str(idx) +'] Execute Stored Procedure from Autnomous Database...')
-            Connection.execute_stored_procedure('utl_sp_load_data_from_adb_to_oci_streaming_poc', var_dml, item['key'], var_value, item['timestamp'])
+            Utility.zipfile(par_oci_adb_wallet_location, par_oci_adb_wallet_temp_dir)            
+            oci_adb.execute_stored_procedure('utl_sp_load_data_from_adb_to_oci_streaming_poc', [var_dml, item['key'], var_value, item['timestamp']])
+            Utility.remove_files_from_directory(par_oci_adb_wallet_temp_dir)
 
     except (Exception, ValueError) as e:
         logger.error(str(e))
@@ -120,72 +113,52 @@ def handler(ctx, data: io.BytesIO=None):
 
     return response.Response(ctx, status_code=200)
 
+# [Class:oci_str] OCI Streaming
+class oci_str():
+    def get_select_before(input_df):
+        before_list = []
+        for col in input_df.columns:
+            if col[0:7] == "before.":
+                before_list.append(col)
+        return input_df[before_list].rename(lambda x: x.replace("before.", ""), axis="columns")
+    
+    def get_select_after(input_df):
+        after_list = []
+        for col in input_df.columns:
+            if col[0:6] == "after.":
+                after_list.append(col)
+        return input_df[after_list].rename(lambda x: x.replace("after.", ""), axis="columns")
 
-# [Class:Connection] Autonomous Database
-class Connection(cx_Oracle.Connection):
-    def __init__(self, *args, cloud_config=None, **kwargs):
-        self.temp_dir = None
-        self.tns_entries = {}
-        if cloud_config is not None:
-            self.__setup_cloud_config(cloud_config)
-            if len(args) > 2:
-                dsn = args[2]
-            else:
-                dsn = kwargs.get('dsn')
-            if dsn in self.tns_entries:
-                dsn = self.tns_entries[dsn]
-                if len(args) > 2:
-                    args = args[:2] + (dsn,) + args[3:]
-                else:
-                    kwargs = kwargs.copy()
-                    kwargs['dsn'] = dsn
-        super(Connection, self).__init__(*args, **kwargs)
+    def set_update_before(table_unique_key, df_before, df_after):
+        df_before.set_index(table_unique_key, inplace=True)
+        df_before.update(df_after.set_index(table_unique_key))
+        return df_before.reset_index()
 
-    def __del__(self):
-        if self.temp_dir is not None:
-            self.temp_dir.cleanup()
-
-    def __setup_cloud_config(self, cloud_config):
-        # extract files in wallet zip to a temporary directory
-        self.temp_dir = tempfile.TemporaryDirectory()
-        zipfile.ZipFile(cloud_config).extractall(self.temp_dir.name)
-
-        # parse tnsnames.ora to get list of entries and modify them to include
-        # the wallet location
-        fname = os.path.join(self.temp_dir.name, 'tnsnames.ora')
-        for line in open(fname):
-            pos = line.find(' = ')
-            if pos < 0:
-                continue
-            name = line[:pos]
-            entry = line[pos + 3:].strip()
-            key_phrase = '(security='
-            pos = entry.find(key_phrase) + len(key_phrase)
-            wallet_entry = '(MY_WALLET_DIRECTORY=%s)' % self.temp_dir.name
-            entry = entry[:pos] + wallet_entry + entry[pos:]
-            self.tns_entries[name] = entry
-
-    def execute_stored_procedure(stored_name, par_dml, par_key, par_value, par_timestamp):
+# [Class:oci_adb] Autonomous Database
+class oci_adb():
+    def execute_stored_procedure(stored_name, par_list):
         logger = logging.getLogger()
         
         try:
             ts = process_time()
-            conn = Connection(par_oci_adb_user_name_secret_ocid,
-                              par_oci_adb_password_secret_ocid,
-                              par_oci_adb_service_name,
-                              cloud_config=par_oci_adb_wallet_location)
+            conn = oracledb.connect(user            = par_oci_adb_user_name_secret_ocid,
+                                    password        = par_oci_adb_password_secret_ocid,
+                                    dsn             = par_oci_adb_service_name,
+                                    config_dir      = par_oci_adb_wallet_temp_dir,
+                                    wallet_location = par_oci_adb_wallet_temp_dir,
+                                    wallet_password = par_oci_adb_wallet_password_secret_ocid)
             
             # create a cursor
             with conn.cursor() as cursor:
                 # execute the insert statement
-                cursor.callproc(stored_name, [par_dml, par_key, par_value, par_timestamp])
+                cursor.callproc(stored_name, par_list)
                 # commit the change
                 conn.commit()
 
             te = process_time()
-            logger.info('[END] Execute Stored Procedure...[DML='+ par_dml +'][seg: '+ str(te-ts) +']...[Succeded]')
+            logger.info('[END] Execute Stored Procedure...[DML='+ par_list[0] +'][seg: '+ str(te-ts) +']...[Succeded]')
 
-        except cx_Oracle.Error as e:
+        except (Exception, ValueError) as e:
             logger.error(str(e))
             raise
 
